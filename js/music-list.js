f0ade5a933a978a6392c34e64f57a737a45af665
Zeidant <anthonyemma55@gmail.com>
2024-02-25 20:43:34 -0400

    initial commit

---
+++ js/music-list.js
@@ -0,0 +1,555 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+const allMusic = [
+  {
+    "name": "Love Story",
+    "artist": "Indila",
+    "img": "music-1",
+    "src": "music-1"
+  },
+  {
+    "name": "condensed milk",
+    "artist": "cyber milk chan",
+    "img": "music-2",
+    "src": "music-2"
+  },
+  {
+    "name": "Maybe My Soulmate Died",
+    "artist": "iamnotshane",
+    "img": "music-3",
+    "src": "music-3"
+  },
+  {
+    "name": "Something Real",
+    "artist": "Post Malone",
+    "img": "music-4",
+    "src": "music-4"
+  },
+  {
+    "name": "Wishful Thinking",
+    "artist": "MOR",
+    "img": "music-5",
+    "src": "music-5"
+  },
+  {
+    "name": "Gemini",
+    "artist": "Paco Versailles",
+    "img": "music-6",
+    "src": "music-6"
+  },
+  {
+    "name": "SPIT IN MY FACE!",
+    "artist": "ThxSoMch",
+    "img": "music-7",
+    "src": "music-7"
+  },
+  {
+    "name": "Different Kind of Lonely",
+    "artist": "Camp Kubrick",
+    "img": "music-8",
+    "src": "music-8"
+  },
+  {
+    "name": "Libre",
+    "artist": "Ricardo Arjona",
+    "img": "music-9",
+    "src": "music-9"
+  },
+  {
+    "name": "Moonlight",
+    "artist": "Agust D",
+    "img": "music-10",
+    "src": "music-10"
+  },
+  {
+    "name": "Never Is Enough",
+    "artist": "iann dior",
+    "img": "music-11",
+    "src": "music-11"
+  },
+  {
+    "name": "Down In Atlanta",
+    "artist": "Pharrell Williams",
+    "img": "music-12",
+    "src": "music-12"
+  },
+  {
+    "name": "Enter Through The Sun",
+    "artist": "Young Empires",
+    "img": "music-13",
+    "src": "music-13"
+  },
+  {
+    "name": "Do It Now",
+    "artist": "Syn Cole",
+    "img": "music-14",
+    "src": "music-14"
+  },
+  {
+    "name": "Agora Hills",
+    "artist": "Doja Cat",
+    "img": "music-15",
+    "src": "music-15"
+  },
+  {
+    "name": "fedora",
+    "artist": "venny",
+    "img": "music-16",
+    "src": "music-16"
+  },
+  {
+    "name": "INVU",
+    "artist": "TAEYEON",
+    "img": "music-17",
+    "src": "music-17"
+  },
+  {
+    "name": "Wish It Was You - Nils Hoffmann Remix",
+    "artist": "Audien",
+    "img": "music-18",
+    "src": "music-18"
+  },
+  {
+    "name": "Goodbye (from the series Arcane League of Legends)",
+    "artist": "Ramsey",
+    "img": "music-19",
+    "src": "music-19"
+  },
+  {
+    "name": "Hate You",
+    "artist": "Poylow",
+    "img": "music-20",
+    "src": "music-20"
+  },
+  {
+    "name": "One Of Your Girls",
+    "artist": "Troye Sivan",
+    "img": "music-21",
+    "src": "music-21"
+  },
+  {
+    "name": "Poznan",
+    "artist": "Low Roar",
+    "img": "music-22",
+    "src": "music-22"
+  },
+  {
+    "name": "new heartbreak",
+    "artist": "sad alex",
+    "img": "music-23",
+    "src": "music-23"
+  },
+  {
+    "name": "Les \"L\u00e0-bas\"",
+    "artist": "Henri Texier",
+    "img": "music-24",
+    "src": "music-24"
+  },
+  {
+    "name": "Major Crimes",
+    "artist": "HEALTH",
+    "img": "music-25",
+    "src": "music-25"
+  },
+  {
+    "name": "When I'm Small",
+    "artist": "Phantogram",
+    "img": "music-26",
+    "src": "music-26"
+  },
+  {
+    "name": "Lovesick Girls",
+    "artist": "BLACKPINK",
+    "img": "music-27",
+    "src": "music-27"
+  },
+  {
+    "name": "Edge of Seventeen - 2016 Remaster",
+    "artist": "Stevie Nicks",
+    "img": "music-28",
+    "src": "music-28"
+  },
+  {
+    "name": "Message In A Bottle",
+    "artist": "The Police",
+    "img": "music-29",
+    "src": "music-29"
+  },
+  {
+    "name": "Speed Drive (From Barbie The Album)",
+    "artist": "Charli XCX",
+    "img": "music-30",
+    "src": "music-30"
+  },
+  {
+    "name": "Only In My Mind",
+    "artist": "Kenya Grace",
+    "img": "music-31",
+    "src": "music-31"
+  },
+  {
+    "name": "Con La Brisa",
+    "artist": "Foudeqush",
+    "img": "music-32",
+    "src": "music-32"
+  },
+  {
+    "name": "Juju Woman",
+    "artist": "Labrinth",
+    "img": "music-33",
+    "src": "music-33"
+  },
+  {
+    "name": "Goat",
+    "artist": "Thekidszn",
+    "img": "music-34",
+    "src": "music-34"
+  },
+  {
+    "name": "Pray 4 Love (feat. The Weeknd)",
+    "artist": "Travis Scott",
+    "img": "music-35",
+    "src": "music-35"
+  },
+  {
+    "name": "Traici\u00f3n",
+    "artist": "Miranda!",
+    "img": "music-36",
+    "src": "music-36"
+  },
+  {
+    "name": "Don't Go Insane",
+    "artist": "DPR IAN",
+    "img": "music-37",
+    "src": "music-37"
+  },
+  {
+    "name": "Exosphere",
+    "artist": "Creo",
+    "img": "music-38",
+    "src": "music-38"
+  },
+  {
+    "name": "We Kept the Lights On",
+    "artist": "Milo Greene",
+    "img": "music-39",
+    "src": "music-39"
+  },
+  {
+    "name": "Shoreditch",
+    "artist": "Vrdnyn",
+    "img": "music-40",
+    "src": "music-40"
+  },
+  {
+    "name": "A Little Trauma Can Be Illuminating, And I'm Shining Like The Sun",
+    "artist": "$uicideboy$",
+    "img": "music-41",
+    "src": "music-41"
+  },
+  {
+    "name": "Tokyo Drifting",
+    "artist": "Glass Animals",
+    "img": "music-42",
+    "src": "music-42"
+  },
+  {
+    "name": "Barricades",
+    "artist": "Hiroyuki Sawano",
+    "img": "music-43",
+    "src": "music-43"
+  },
+  {
+    "name": "Please Don't Change (feat. DJ Snake)",
+    "artist": "Jung Kook",
+    "img": "music-44",
+    "src": "music-44"
+  },
+  {
+    "name": "Lost in Silence (Aphelios' Song)",
+    "artist": "Marcus Skeen",
+    "img": "music-45",
+    "src": "music-45"
+  },
+  {
+    "name": "TOO MUCH",
+    "artist": "The Kid LAROI",
+    "img": "music-46",
+    "src": "music-46"
+  },
+  {
+    "name": "Other Boys",
+    "artist": "Marshmello",
+    "img": "music-47",
+    "src": "music-47"
+  },
+  {
+    "name": "I Wish I Wasn't Me",
+    "artist": "Eric Nam",
+    "img": "music-48",
+    "src": "music-48"
+  },
+  {
+    "name": "Bad Omens",
+    "artist": "5 Seconds of Summer",
+    "img": "music-49",
+    "src": "music-49"
+  },
+  {
+    "name": "Never Ending Song",
+    "artist": "Conan Gray",
+    "img": "music-50",
+    "src": "music-50"
+  },
+  {
+    "name": "Stuttering",
+    "artist": "Jack & Jack",
+    "img": "music-51",
+    "src": "music-51"
+  },
+  {
+    "name": "Shy Away",
+    "artist": "Twenty One Pilots",
+    "img": "music-52",
+    "src": "music-52"
+  },
+  {
+    "name": "Last Valentines",
+    "artist": "PinkPantheress",
+    "img": "music-53",
+    "src": "music-53"
+  },
+  {
+    "name": "Ma Vie (feat. Yay)",
+    "artist": "Kidd Keo",
+    "img": "music-54",
+    "src": "music-54"
+  },
+  {
+    "name": "FATAL FANTASSY",
+    "artist": "Alvaro Diaz",
+    "img": "music-55",
+    "src": "music-55"
+  },
+  {
+    "name": "Miss You (with Major Lazer & Tory Lanez)",
+    "artist": "Cashmere Cat",
+    "img": "music-56",
+    "src": "music-56"
+  },
+  {
+    "name": "Wait",
+    "artist": "Maroon 5",
+    "img": "music-57",
+    "src": "music-57"
+  },
+  {
+    "name": "En M\u00ed",
+    "artist": "J Balvin",
+    "img": "music-58",
+    "src": "music-58"
+  },
+  {
+    "name": "South of the Border (feat. Camila Cabello & Cardi B)",
+    "artist": "Ed Sheeran",
+    "img": "music-59",
+    "src": "music-59"
+  },
+  {
+    "name": "Fuck Shit",
+    "artist": "870glizzy",
+    "img": "music-60",
+    "src": "music-60"
+  },
+  {
+    "name": "TALK2ME",
+    "artist": "Mike Dimes",
+    "img": "music-61",
+    "src": "music-61"
+  },
+  {
+    "name": "No Te Veo - Remix",
+    "artist": "Casa De Leones",
+    "img": "music-62",
+    "src": "music-62"
+  },
+  {
+    "name": "Boys",
+    "artist": "Charli XCX",
+    "img": "music-63",
+    "src": "music-63"
+  },
+  {
+    "name": "Dream of You",
+    "artist": "Camila Cabello",
+    "img": "music-64",
+    "src": "music-64"
+  },
+  {
+    "name": "it's not u it's me",
+    "artist": "Bea Miller",
+    "img": "music-65",
+    "src": "music-65"
+  },
+  {
+    "name": "Boy With Luv (feat. Halsey)",
+    "artist": "BTS",
+    "img": "music-66",
+    "src": "music-66"
+  },
+  {
+    "name": "Comfortable",
+    "artist": "Victor Ray",
+    "img": "music-67",
+    "src": "music-67"
+  },
+  {
+    "name": "Better to Lie (with Jesse & Swae Lee)",
+    "artist": "benny blanco",
+    "img": "music-68",
+    "src": "music-68"
+  },
+  {
+    "name": "Pope Is a Rockstar",
+    "artist": "SALES",
+    "img": "music-69",
+    "src": "music-69"
+  },
+  {
+    "name": "ten",
+    "artist": "Fred again..",
+    "img": "music-70",
+    "src": "music-70"
+  },
+  {
+    "name": "The Color Violet",
+    "artist": "Tory Lanez",
+    "img": "music-71",
+    "src": "music-71"
+  },
+  {
+    "name": "Babylon",
+    "artist": "5 Seconds of Summer",
+    "img": "music-72",
+    "src": "music-72"
+  },
+  {
+    "name": "BARBIEZ",
+    "artist": "Fuerza Regida",
+    "img": "music-73",
+    "src": "music-73"
+  },
+  {
+    "name": "On My Own",
+    "artist": "Jaden",
+    "img": "music-74",
+    "src": "music-74"
+  },
+  {
+    "name": "Forget Me Nots",
+    "artist": "Patrice Rushen",
+    "img": "music-75",
+    "src": "music-75"
+  },
+  {
+    "name": "Candy",
+    "artist": "Doja Cat",
+    "img": "music-76",
+    "src": "music-76"
+  },
+  {
+    "name": "Don't Go Yet",
+    "artist": "Camila Cabello",
+    "img": "music-77",
+    "src": "music-77"
+  },
+  {
+    "name": "en guerra",
+    "artist": "paopao",
+    "img": "music-78",
+    "src": "music-78"
+  },
+  {
+    "name": "Make You Mine",
+    "artist": "Madison Beer",
+    "img": "music-79",
+    "src": "music-79"
+  },
+  {
+    "name": "Aria Math",
+    "artist": "C418",
+    "img": "music-80",
+    "src": "music-80"
+  },
+  {
+    "name": "psychofreak (feat. WILLOW)",
+    "artist": "Camila Cabello",
+    "img": "music-81",
+    "src": "music-81"
+  },
+  {
+    "name": "MAMBO 23",
+    "artist": "Juan Luis Guerra 4.40",
+    "img": "music-82",
+    "src": "music-82"
+  },
+  {
+    "name": "Ninety",
+    "artist": "Jaden",
+    "img": "music-83",
+    "src": "music-83"
+  },
+  {
+    "name": "Bad Luck",
+    "artist": "Khalid",
+    "img": "music-84",
+    "src": "music-84"
+  },
+  {
+    "name": "BACK TO ME",
+    "artist": "\u00a5$",
+    "img": "music-85",
+    "src": "music-85"
+  },
+  {
+    "name": "2U (feat. Justin Bieber)",
+    "artist": "David Guetta",
+    "img": "music-86",
+    "src": "music-86"
+  },
+  {
+    "name": "Roxanne",
+    "artist": "The Police",
+    "img": "music-87",
+    "src": "music-87"
+  },
+  {
+    "name": "Amor de Lejos Remix",
+    "artist": "Baby Rasta & Gringo",
+    "img": "music-88",
+    "src": "music-88"
+  },
+  {
+    "name": "Ashley",
+    "artist": "Halsey",
+    "img": "music-89",
+    "src": "music-89"
+  },
+  {
+    "name": "9 (After Coachella) (feat. M\u00d8 & SOPHIE)",
+    "artist": "Cashmere Cat",
+    "img": "music-90",
+    "src": "music-90"
+  }
+];
+
+export default allMusic;
