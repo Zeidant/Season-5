b5b0592e9fd3e7059ea20c9eefd61548adc62361
Zeidant <anthonyemma55@gmail.com>
2024-03-03 19:25:34 -0400

    Adding song day 3

--- js/music-list.js
+++ js/music-list.js
@@ -11,6 +11,12 @@ const allMusic = [
   "img": "music-2",
   "src": "music-2"
 }
+{
+"name": "3. Maybe My Soulmate Died",
+"artist": "iamnotshane",
+"img": "music-3",
+"src": "music-3"
+}
   ];
 
 export default allMusic;
