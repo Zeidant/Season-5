c11de15eb20de1e7012b22df527ef0a18a47d961
Zeidant <anthonyemma55@gmail.com>
2024-04-04 22:29:15 -0400

    Adding song day 35

--- js/music-list.js
+++ js/music-list.js
@@ -203,6 +203,12 @@ const allMusic = [
   "img": "music-34",
   "src": "music-34"
 },
+{
+  "name": "35. Traici\u00f3n",
+  "artist": "Miranda!",
+  "img": "music-35",
+  "src": "music-35"
+},
   ];
 
 export default allMusic;
