e125c9903afd4ac9dd0ad1285dd11d97d9b2b126
Zeidant <anthonyemma55@gmail.com>
2024-05-02 23:00:07 -0400

    Adding song day 62

--- js/music-list.js
+++ js/music-list.js
@@ -365,6 +365,12 @@ const allMusic = [
   "img": "music-61",
   "src": "music-61"
 },
+{
+  "name": "62. Boys",
+  "artist": "Charli XCX",
+  "img": "music-62",
+  "src": "music-62"
+},
   ];
 
 export default allMusic;
